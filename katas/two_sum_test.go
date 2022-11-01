f70f02a957604dd74f9e384479ffda8627ad5ec8
eliflores <eli@mignonnesaurus.com>
2022-11-01 21:35:26 +0100

    First solution to the Two Sum Kata
    Results from LeetCode:
    ```
    Runtime: 46 ms
    Memory Usage: 3.6 MB
    ```

---
+++ katas/two_sum_test.go
@@ -0,0 +1,61 @@
+package katas
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+
+	tests := []struct {
+		nums    []int
+		target  int
+		indices []int
+	}{
+		{
+			[]int{},
+			0,
+			[]int{},
+		},
+		{
+			[]int{1},
+			0,
+			[]int{},
+		},
+		{
+			[]int{3, 3},
+			6,
+			[]int{0, 1},
+		},
+		{
+			[]int{3, 2, 4},
+			6,
+			[]int{1, 2},
+		},
+		{
+			[]int{2, 7, 11, 15},
+			9,
+			[]int{0, 1},
+		},
+		{
+
+			[]int{-1, -2, -3, -4, -5},
+			-8,
+			[]int{2, 4},
+		},
+
+		{
+			[]int{3, 3, 3},
+			5,
+			[]int{},
+		},
+	}
+
+	for _, test := range tests {
+		indices := twoSum(test.nums, test.target)
+
+		if !reflect.DeepEqual(indices, test.indices) {
+			t.Errorf("for nums %v and target = %d -> indices = %v; want %v", test.nums, test.target, indices, test.indices)
+		}
+	}
+}
