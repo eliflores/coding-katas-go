a96ae4e943e332958680fb85e44e209fcdda2762
eliflores <eli@mignonnesaurus.com>
2022-11-01 22:54:24 +0100

    Remove named return parameter
    And simply return nil at the end, that is the zero value of a slice.

--- katas/two_sum.go
+++ katas/two_sum.go
@@ -7,7 +7,7 @@ package katas
 // twoSum returns indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
-func twoSum(nums []int, target int) (result []int) {
+func twoSum(nums []int, target int) []int {
 	numToIdx := make(map[int]int)
 
 	for i, num := range nums {
@@ -17,5 +17,5 @@ func twoSum(nums []int, target int) (result []int) {
 		numToIdx[nums[i]] = i
 	}
 
-	return
+	return nil
 }
